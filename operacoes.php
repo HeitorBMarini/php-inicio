f55813ee40feff026f38bc936ccfac616c2216f1
HeitorBMarini <mariniheitor@gmail.com>
2023-05-16 07:59:00 -0300

    Operações com php

---
+++ operacoes.php
@@ -0,0 +1,19 @@
+<?php
+
+$idade = 19;
+
+$idadeDaqui10anos = $idade ** 10;
+
+
+
+$soma = 2 + 2;
+$subtracao = 2 - 2;
+$multiplicacao = 2 * 2;
+$divisao = 2 / 2;
+
+$doisAoCubo = 2 ** 3;
+//ou
+$doisAoCubo = 2 * 2 * 2;
+
+$restoDaDivisao = 10 % 3;
+echo $restoDaDivisao;
