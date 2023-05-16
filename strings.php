19d777b57c0a4585467e61a5a70d9b219dd590e7
HeitorBMarini <mariniheitor@gmail.com>
2023-05-16 08:35:42 -0300

    vendo como concatenar e quebrar linha

---
+++ strings.php
@@ -0,0 +1,10 @@
+<?php
+
+$idade = 19;
+echo "Me chamo Heitor\n"; // \n quebra a linha
+
+echo "Zé pequeno" . PHP_EOL ;// eol = end of line (quebra linha tambem)
+
+echo "minha idade é  $idade  anos";// . ( nome variavel) para concatenar
+
+echo " \t tudo nosso"; // \t da um tab
